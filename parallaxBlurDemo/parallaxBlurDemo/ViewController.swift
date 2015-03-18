283f1cfe32d6a18a812de59c056bd8dea533e82d
Anoop tomar <anooptomar@Anoops-macbook.local>
2015-03-17 18:41:21 -0700

    parallax

---
+++ parallaxBlurDemo/parallaxBlurDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  parallaxBlurDemo
+//
+//  Created by Anoop tomar on 3/17/15.
+//  Copyright (c) 2015 Anoop tomar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
