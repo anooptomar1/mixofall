0af6661de43d8ad5b023b33157d3520a0a6a4f33
Anoop tomar <anooptomar@Anoops-macbook.T-mobile.com>
2015-10-09 22:21:08 -0700

    Swift2 demo apps

---
+++ DemoProjects/DemoProjects/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoProjects
+//
+//  Created by Anoop tomar on 10/9/15.
+//  Copyright © 2015 Anoop tomar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
